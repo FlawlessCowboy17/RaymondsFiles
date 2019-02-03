347689ee25d22ad8ca55b3b57205e34cb7339436
FlawlessCowboy17 <47056343+FlawlessCowboy17@users.noreply.github.com>
2019-02-03 09:36:04 -0500

    update challenge 1

--- Assets/script/playercontrol1.cs
+++ Assets/script/playercontrol1.cs
@@ -7,17 +7,23 @@ public class playercontrol1 : MonoBehaviour
 {
     public float speed;
     public Text countText;
+    public Text livesText;
     public Text winText;
+    public Text loseText;
 
     private Rigidbody rb;
     private int count;
+    private int lives;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         count = 0;
         SetcountText();
+        lives = 3;
+        SetlivesText();
         winText.text = "";
+        loseText.text = "";
     }
 
     void FixedUpdate()
@@ -33,18 +39,49 @@ public class playercontrol1 : MonoBehaviour
     }
     void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.CompareTag("Pick Up"))
+        if (other.gameObject.CompareTag("Pick Up"))
         {
             other.gameObject.SetActive(false);
             count = count + 1;
             SetcountText();
+        if(count == 12)
+            {
+                teleport();
+            }
+        }
+        else if(other.gameObject.CompareTag("bad up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count - 1;
+            SetcountText();
+            lives = lives - 1;
+            SetlivesText();
+            if (lives <= 0)
+            {
+                DestroyGameObject();
+            }
         }
     }
     void SetcountText()
     {
         countText.text = "Count: " + count.ToString();
-        if (count >= 12) {
+        if (count >= 20) {
             winText.text = "You win!";
                 }
+    }
+    void SetlivesText()
+    {
+        livesText.text = "Lives: " + lives.ToString() + "/3";
+        if (lives <= 0) {
+            loseText.text = "You lose!";
+        }
+    }
+    void teleport()
+    {
+        transform.position = new Vector3(100, 1.5f, 14);
+    }
+    void DestroyGameObject()
+    {
+        gameObject.SetActive(false);
     }
 }
